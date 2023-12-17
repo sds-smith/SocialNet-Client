df406ad2de0c7b12375e4da878d11d4230b5de33
Shawn Smith <sds.smith24@gmail.com>
2023-12-17 18:33:19 -0500

    add useEffect dummy backend call

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import {useEffect} from 'react'
 
 export default function App() {
+
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch('http://localhost:80')
+      const r = await resp.json();
+      console.log(r)
+    })()
+      
+  }, [])
+
   return (
     <div>App</div>
   )
