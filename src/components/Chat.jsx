95d7e1b9123c1f990f867d84586c264b222efa80
Shawn Smith <sds.smith24@gmail.com>
2024-01-02 17:41:35 -0500

    implement MessageInput and MessageList in Chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,6 @@
-import { useGreeting } from '../utils/hooks/apollo.hooks'
+import { useGreeting } from '../utils/hooks/apollo.hooks';
+import MessageInput from './MessageInput';
+import MessageList from './MessageList';
 
 export default function Chat({ user }) {
 
@@ -7,6 +9,15 @@ export default function Chat({ user }) {
     return (
         <>
             <div>{`${user} ${greeting}` || 'Chat'}</div>
+            <section className="section">
+              <div className="container">
+                <h1 className="title is-4">
+                  {`Chatting as ${user}`}
+                </h1>
+                <MessageList user={user}/>
+                <MessageInput />
+              </div>
+            </section>
         </>
     )
 }
