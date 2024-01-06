5a13a7b7b6e99db858bf1d9d49474c6acfac76a2
Shawn Smith <sds.smith24@gmail.com>
2024-01-06 09:16:08 -0500

    implement authLink in apollo client

--- src/utils/graphql/apollo-client.js
+++ src/utils/graphql/apollo-client.js
@@ -1,16 +1,23 @@
-import {
-    ApolloClient,
-    InMemoryCache,
-    gql
-  } from "@apollo/client";
-  import { greetingQuery } from "./queries";
+import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Kind, OperationTypeNode } from 'graphql';
+// import { createClient as createWsClient } from 'graphql-ws';
+import { getAccessToken } from '../auth';
+
+const authLink = new ApolloLink((operation, forward) => {
+  const accessToken = getAccessToken();
+  if (accessToken) {
+    operation.setContext({
+      headers: { 'Authorization': `Bearer ${accessToken}` }
+    });
+  };
+  return forward(operation);
+});
+
+const httpLink = concat(authLink, createHttpLink({ uri: 'http://localhost:80/graphql' }));
 
   export const apolloClient = new ApolloClient({
-    uri: 'http://localhost:80/graphql',
+    link: httpLink,
     cache: new InMemoryCache()
   });
-
-export async function getGreeting() {
-  const { data } = await apolloClient.query({query : greetingQuery});
-  console.log(data.greeting);
-}
