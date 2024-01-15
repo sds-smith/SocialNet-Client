ae8433c8e185d0b159d4350574542b9841e7cf9a
Shawn Smith <sds.smith24@gmail.com>
2024-01-15 17:34:20 -0500

    remove apollo hook from user context

--- src/context/user-context.js
+++ src/context/user-context.js
@@ -1,23 +1,19 @@
 import { useState, createContext } from "react";
-import { useUser } from "../utils/hooks/apollo.hooks";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({children}) => {
     const [authenticatedUser, setAuthenticatedUser] = useState(null);
 
-    const user = useUser();
-
     const authUserExists = authenticatedUser?.uid?.length > 0;
 
     async function setUser(signedInUser) {
-        const userResponse = await user({
+        setAuthenticatedUser({
             displayName : signedInUser.displayName,
             email : signedInUser.email,
             photoURL : signedInUser.photoURL,
             uid : signedInUser.uid,
         })
-        setAuthenticatedUser(userResponse)
     }
 
     function logoutUser() {
