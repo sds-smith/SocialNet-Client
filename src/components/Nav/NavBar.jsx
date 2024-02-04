217614827a1b28a713b70dbaa37360189e4c9cc1
Shawn Smith <sds.smith24@gmail.com>
2024-02-04 11:18:01 -0500

    add responsive styling to Nav component

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -12,21 +12,23 @@ export default function NavBar() {
   return (
     <Box sx={classes.navContainer}>
       <Grid container>
-        <Grid container item xs={8}>
-          <Grid item xs={2}>
-            <Typography variant='h4' component='h1' >
+        <Grid container item xs={9} lg={8}>
+          <Grid item xs={12} lg={2}>
+            <Typography variant={{xs: 'h6', lg:'h4'}} component='h1' >
               CUP
             </Typography>
           </Grid>
           <Grid item xs={10}>
-            <Typography variant='h6' component='h4' >
+            <Typography variant={{xs: 'body2', lg:'h6'}} component='h4' >
               A Social App for Coffee Lovers
             </Typography>
           </Grid>
         </Grid>
+        <Grid item xs={3} lg={4}>
           <Button variant='contained'  onClick={logoutUser} disabled={!authUserExists}>
             LOGOUT
           </Button >
+        </Grid>
       </Grid>
     </Box>
   );
